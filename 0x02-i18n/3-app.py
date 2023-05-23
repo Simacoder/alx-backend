4d714d62245c8d0811eeb7facebab11638213098
Simanga <simacoder@hotmail.com>
2023-05-23 22:10:08 +0000

    errors fixing

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 """
-Basic Flask app module.
+Parametrize templates
 """
+
+import babel
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
+from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config(object):
-    """ Language and time zone settings.
+class Config:
+    """
+    Config class
     """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
@@ -20,19 +23,21 @@ class Config(object):
 app.config.from_object(Config)
 
 
-@app.route('/')
-def hello():
-    """ Hello method.
-    """
-    return render_template('3-index.html')
-
-
 @babel.localeselector
 def get_locale():
-    """ Method to determine the best match with our supported languages.
+    """
+     determine the best match with our supported languages.
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """
+    hello world
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
