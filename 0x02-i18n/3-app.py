dfd5898395304b1201f830754f695f0da2a2f309
Simanga <simacoder@hotmail.com>
2023-05-23 22:04:13 +0000

    task 3 fix

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python3
 """
-Parametrize templates
+Basic Flask app module.
 """
-
-import babel
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, gettext
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config:
-    """
-    Config class
+class Config(object):
+    """ Language and time zone settings.
     """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
@@ -23,21 +20,19 @@ class Config:
 app.config.from_object(Config)
 
 
-@babel.localeselector
-def get_locale():
-    """
-     determine the best match with our supported languages.
-    """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-@app.route('/', methods=['GET'], strict_slashes=False)
-def index():
-    """
-    hello world
+@app.route('/')
+def hello():
+    """ Hello method.
     """
     return render_template('3-index.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@babel.localeselector
+def get_locale():
+    """ Method to determine the best match with our supported languages.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
